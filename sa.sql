53148d1d0d1a68a8b1eb85abf570d3098b467e34
Amaury <amaury.francois@gmail.com>
2014-10-25 18:49:01 +0200

    Create sa.sql

---
+++ sa.sql
@@ -0,0 +1,12 @@
+set lines 200
+set pages 200
+col machine format a30
+select sid,
+serial#,
+program,
+machine,
+status,
+wait_class
+from v$session
+where type <> 'BACKGROUND'
+and status='ACTIVE';
