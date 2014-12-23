989a0a7f16fb58069feb9ab60dbfd53b7460ce5b
Amaury <amaury.francois@gmail.com>
2014-12-23 18:06:31 +0100

    Update inv_obj.sql

--- inv_obj.sql
+++ inv_obj.sql
@@ -1,11 +1,13 @@
 set pages 200
 set lines 200
 
-col owner format a30
-col object_name format a100
+col owner format a20
+col object_type format a20
+col object_name format a60
 
 select
 owner,
+object_type,
 object_name
 from
 dba_objects
