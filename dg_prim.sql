7cb8b27d2ce4696c8c118d4120e7b491a8c4dabc
Amaury <amaury.francois@gmail.com>
2014-12-23 17:55:38 +0100

    Update dg_prim.sql

--- dg_prim.sql
+++ dg_prim.sql
@@ -2,12 +2,14 @@ set lines 200
 set pages 200
 col dest_name format a20
 col status format a10
+col gap_status format a20
 select
 inst_id,
 dest_name,
 status,
 protection_mode,
 ARCHIVED_SEQ#,
-APPLIED_SEQ#
+APPLIED_SEQ#,
+gap_status
 from gv$archive_dest_status
 where status <> 'INACTIVE';
