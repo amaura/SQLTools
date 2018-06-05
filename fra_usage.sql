09008bcf795b91b440609a0cd8a57af7df1eb88f
Amaury <amaury.francois@gmail.com>
2018-06-05 11:01:09 +0200

    Create fra_usage.sql

---
+++ fra_usage.sql
@@ -0,0 +1,10 @@
+set lines 200
+col 
+col "TOTAL GB" format 99999.9
+col "USED GB" format 99999.9
+col "RECLAIMABLE GB" format 99999.9
+space_limit/1024/1024/1024 "TOTAL GB",
+SPACE_USED/1024/1024/1024 "USED GB",
+SPACE_RECLAIMABLE/1024/1024/1024 "RECLAIMABLE GB" 
+from 
+v$recovery_file_dest;
