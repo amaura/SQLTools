cbb7068d8f8b4ddc52c10f10039c9b7ba88954a0
Amaury <amaury.francois@gmail.com>
2023-01-31 09:35:23 +0100

    Create pdb_tde.sql

---
+++ pdb_tde.sql
@@ -0,0 +1,6 @@
+set lines 200
+col name format a20
+col key_id format a40
+col keystore_type format a30
+
+select p.name,e.KEY_ID,e.KEYSTORE_TYPE from v$pdbs p left join v$encryption_keys e on p.con_id=e.con_id;
