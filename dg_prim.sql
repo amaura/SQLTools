92a331894a60c48f9a3aae31625a75d7dcb39f6f
Amaury <amaury.francois@gmail.com>
2023-02-15 15:30:55 +0100

    Update dg_prim.sql

--- dg_prim.sql
+++ dg_prim.sql
@@ -3,6 +3,7 @@ set pages 200
 col dest_name format a20
 col status format a10
 col gap_status format a20
+col error format a80
 select
 inst_id,
 dest_name,
@@ -10,7 +11,8 @@ status,
 protection_mode,
 ARCHIVED_SEQ#,
 APPLIED_SEQ#,
-gap_status
+gap_status,
+error
 from gv$archive_dest_status
 where status <> 'INACTIVE'
 order by 1,2;
