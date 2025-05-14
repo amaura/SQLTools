f1d948854d4e31be6d1347bea69d051d9d0858c4
Amaury FRANCOIS <amaury.francois@gmail.com>
2025-05-14 23:37:27 +0200

    Update rman_throughput.sql

--- rman_throughput.sql
+++ rman_throughput.sql
@@ -1,12 +1,12 @@
 SET HEAD OFF
 SELECT 'Throughput: '||
-       ROUND(SUM(v.value/1024/1024/1024),1) || ' GB so far @ ' ||
+       ROUND(SUM(v.value/1024/1024/1024),3) || ' GB so far @ ' ||
        ROUND(SUM(v.value     /1024/1024/1024)/NVL((SELECT MIN(elapsed_seconds)
             FROM gv$session_longops
             WHERE opname          LIKE 'RMAN: aggregate input'
               AND sofar           != TOTALWORK
               AND elapsed_seconds IS NOT NULL
-       ),SUM(v.value     /1024/1024)),2) || ' GB/sec'
+       ),SUM(v.value     /1024/1024)),3) || ' GB/sec'
  FROM gv$sesstat v, v$statname n, gv$session s
 WHERE v.statistic# = n.statistic#
   AND n.name       = 'physical write total bytes'
